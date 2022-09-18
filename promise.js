d76d8afed4353fdc8b5eff09e6bf0bab2ad630f9
Maulana Krisna Adhitya <maulanakrisna@pln.co.id>
2022-09-18 22:41:07 +0800

    pusing, ntar dulu

--- promise.js
+++ promise.js
@@ -1,8 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async() => {
+  promiseTheaterIXX = await promiseTheaterIXX();
+  promiseTheaterVGC = await promiseTheaterVGC();
+
+};
 
 module.exports = {
   promiseOutput,
-};
+}

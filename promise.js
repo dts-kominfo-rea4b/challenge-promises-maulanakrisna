7df18528e81286b39ce74d3ff8ee4e4b2661cc39
Maulana Krisna Adhitya <maulanakrisna@pln.co.id>
2022-09-21 10:27:53 +0800

    hasil keluaran sudah sesuai

--- promise.js
+++ promise.js
@@ -1,14 +1,45 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC} = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = async() => {
-  try{
-    const promiseTheaterIXX = await promiseTheaterIXX();
-    const promiseTheaterVGC = await promiseTheaterVGC();
-  } catch (err){
-    console.log(err);
+const promiseOutput = async (keyword) => {
+  
+
+  let jumlahMarah = 0;
+  let jumlahTidakMarah = 0;
+  const filmTheaterIXX = await promiseTheaterIXX();
+  const filmTheaterVGC = await promiseTheaterVGC();
+ 
+  filmTheaterIXX.forEach(element => {
+    if (element.hasil == 'marah'){
+      jumlahMarah += 1;
+    }
+    else if (element.hasil == 'tidak marah'){
+      jumlahTidakMarah += 1;
+    } 
+  });
+
+  filmTheaterVGC.forEach(element => {
+    if (element.hasil == 'marah'){
+      jumlahMarah += 1;
+    }
+    else if (element.hasil == 'tidak marah'){
+      jumlahTidakMarah += 1;
+    } 
+  });
+
+  // console.log(`${jumlahMarah}, ${jumlahTidakMarah}`)
+
+  if (keyword == 'marah') {
+    return jumlahMarah;
+  } else if (keyword == 'tidak marah') {
+    return jumlahTidakMarah;
+  } else {
+    return null;
   }
+
+ 
 };
+
 
 module.exports = {
   promiseOutput,
